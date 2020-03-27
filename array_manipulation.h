d8d590da3c95b063b81b823ddac64f8854557220
sixthkrum <mailbox.vikramalagh@gmail.com>
2020-03-27 17:50:48 +0530

    added header guards

--- array_manipulation.h
+++ array_manipulation.h
@@ -1,3 +1,6 @@
+#ifndef ARRAY_MANIPULATION_H
+#define ARRAY_MANIPULATION_H
+
 int components_move_up(int numarr[4][4]);//moves components up until they reach the boundary or they touch another component uppermost components are moved first and empty spaces have zeros in them if two similar components touch then the bottom most one dissapears and the uppermost one is doubled
 
 int components_move_down(int numarr[4][4]);
@@ -7,3 +10,5 @@ int components_move_left(int numarr[4][4]);
 int components_move_right(int numarr[4][4]);
 
 int check_if_possible_to_move(int numarr[4][4]);//only runs if empty_spaces is equal to zero otherwise does not run
+
+#endif
--- graphical_functions.h
+++ graphical_functions.h
@@ -1,3 +1,6 @@
+#ifndef GRAPHICAL_FUNCTIONS_H
+#define GRAPHICAL_FUNCTIONS_H
+
 int matrix_make();//assumes initscr raw noecho
 
 int numbers_set(int numarr[4][4]);//sets numbers from array into matrix//assumes initscr raw noecho
@@ -9,3 +12,5 @@ int empty_cell(int y, int x);
 int fill_cell(int num, int y, int x);
 
 int insert_new_number(int numarr[4][4], int empty_spaces);
+
+#endif
